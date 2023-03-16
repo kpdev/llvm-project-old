ae08106dba901843165c967983a26e790659afee
Pavel Kosov <kpdev42@gmail.com>
2023-03-16 16:26:54 +0300

    [PP-EXT] Add functions to a test file

--- clang/test/CodeGen/pp-generalization.c
+++ clang/test/CodeGen/pp-generalization.c
@@ -11,12 +11,15 @@
 // CHECK-LL: struct Generalization definition
 // CHECK-LL: load 'double'
 // CHECK-LL: __pp_specialization_type 'int'
-// CHECK-LL: %struct.__pp_struct_Generalization__Base1 = type { %struct.Generalization, %struct.Base1 }
-// CHECK-LL: %struct.Generalization = type { double, i32 }
+
 // CHECK-LL: %struct.Base1 = type { i32 }
-// CHECK-LL: %struct.__pp_struct_Generalization__Base2 = type { %struct.Generalization, %struct.Base2 }
 // CHECK-LL: %struct.Base2 = type { i32 }
-// CHECK-LL: double @check_gen(ptr noundef byval(%struct.__pp_struct_Generalization__Base1) align 8 %gb)
+// CHECK-LL: %struct.Generalization = type { double, i32 }
+// CHECK-LL: %struct.__pp_struct_Generalization__Base1 = type { %struct.Generalization, %struct.Base1 }
+// CHECK-LL: %struct.__pp_struct_Generalization__Base2 = type { %struct.Generalization, %struct.Base2 }
+
+// CHECK-LL: double @check_gen_b1(ptr noundef byval(%struct.__pp_struct_Generalization__Base1) align 8 %gb)
+// CHECK-LL: double @check_gen_b2(ptr noundef byval(%struct.__pp_struct_Generalization__Base2) align 8 %gb)
 
 // CHECK-AST:     |-RecordDecl {{.*}} struct Base1 definition
 // CHECK-AST-NEXT:| `-FieldDecl {{.*}} i 'int'
@@ -36,15 +39,31 @@ struct Base1 { int i; };
 struct Base2 { int j; };
 struct Generalization { double load; } < Base1, Base2 >;
 
-double check_gen (struct Generalization<Base1> gb) { return gb.__pp_head.load; }
+int get_b1_field(struct Base1 b) { return b.i; }
+int get_b2_field(struct Base2 b) { return b.j; }
 double foo (struct Generalization g) { return g.load; }
 
 int get_tag1 (struct __pp_struct_Generalization__Base1 b)
     { return b.__pp_head.__pp_specialization_type; }
 int get_tag2 (struct __pp_struct_Generalization__Base2 b)
     { return b.__pp_head.__pp_specialization_type; }
+
 struct Generalization get_gen1 (struct __pp_struct_Generalization__Base1 b) { return b.__pp_head; }
 struct Generalization get_gen2 (struct __pp_struct_Generalization__Base2 b) { return b.__pp_head; }
 
 struct Base1 get_b1 (struct __pp_struct_Generalization__Base1 b) { return b.__pp_tail; }
 struct Base2 get_b2 (struct __pp_struct_Generalization__Base2 b) { return b.__pp_tail; }
+
+double check_gen_b1 (struct Generalization<Base1> gb) { return gb.__pp_head.load; }
+double check_gen_b2 (struct Generalization<Base2> gb) { return gb.__pp_head.load; }
+
+int get_spec_field1 (struct Generalization<Base1> gb) { return gb.__pp_tail.i; }
+int get_spec_field2 (struct Generalization<Base2> gb) { return gb.__pp_tail.j; }
+
+int test_vars() {
+    struct Generalization<Base1> gb1;
+    gb1.__pp_tail.i = 3;
+    struct Generalization<Base2> gb2;
+    gb2.__pp_tail.j = 5;
+    return get_spec_field1(gb1) + get_spec_field2(gb2);
+}
