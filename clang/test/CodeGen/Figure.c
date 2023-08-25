e7366ad4d897a60c21633024f7e3ee6853f24d0d
Pavel Kosov <kpdev42@gmail.com>
2023-08-25 11:14:31 +0300

    [PP_EXT] Cleanup test

--- clang/test/CodeGen/Figure.c
+++ clang/test/CodeGen/Figure.c
@@ -68,7 +68,7 @@
 // CHECK-AST-NEXT:| `-CompoundStmt {{.*}}
 
 
-// //---------------------------------------
+//---------------------------------------
 
 #include <stdio.h>
 
@@ -153,79 +153,3 @@ int main() {
     // CHECK-RT-NEXT: BaseObject<NewObject>: 101 102
     printf("BaseObject<NewObject>: %d %d\n", obj.a, obj<b>);
 }
-
-//----------------------------------------
-
-// struct Base1 { int i; };
-// struct Base2 { int j; };
-// struct Generalization { double load; } < Base1, Base2 >;
-
-// int get_b1_field(struct Base1 b) { return b.i; }
-// int get_b2_field(struct Base2 b) { return b.j; }
-// double foo (struct Generalization g) { return g.load; }
-
-// int get_tag1 (struct __pp_struct_Generalization__Base1 b)
-//     { return b.__pp_specialization_type; }
-// int get_tag2 (struct __pp_struct_Generalization__Base2 b)
-//     { return b.__pp_specialization_type; }
-
-// void test_field_access() {
-//     struct Generalization<Base1> gb;
-//     gb<i> = 42;
-//     gb.load = 3.0;
-// }
-
-// // struct Generalization get_gen1 (struct __pp_struct_Generalization__Base1 b) { return b.__pp_head; }
-// // struct Generalization get_gen2 (struct __pp_struct_Generalization__Base2 b) { return b.__pp_head; }
-
-// struct Base1 get_b1 (struct Generalization<Base1> b) { return b<>; }
-// struct Base2 get_b2 (struct Generalization<Base2> b) { return b<>; }
-
-// double check_gen_b1 (struct Generalization<Base1> gb) { return gb.load; }
-// double check_gen_b2 (struct Generalization<Base2> gb) { return gb.load; }
-
-// int get_spec_field1 (struct Generalization<Base1> gb) { return gb<i>; }
-// int get_spec_field2 (struct Generalization<Base2> gb) { return gb<j>; }
-
-// int test_vars() {
-//     struct Generalization<Base1> gb1;
-//     gb1<i> = 3;
-//     struct Generalization<Base2> gb2;
-//     gb2<j> = 5;
-//     return get_spec_field1(gb1) + get_spec_field2(gb2);
-// }
-
-// struct Base_gen1 { struct Generalization<Base1> b; };
-// struct Base_gen2 { struct Generalization<Base2> b; };
-// // struct Generalization_nested { double additional_field; } < Generalization<Base1>, Generalization<Base2> >;
-// struct Generalization_extended { double other_field; } < Base_gen1, Base_gen2 >;
-
-// void test_nested_vars() {
-//     // struct Generalization_nested<Generalization<Base1>> gn1;
-//     // gn1<><i> = 4;
-//     struct Generalization_extended<Base_gen1> ge1;
-//     ge1<b><i> = 5;
-// }
-
-#if 0
-#if 1
-
-struct Circle { int r; };
-struct Rectangle { int w, h; };
-struct Figure { unsigned color; } < struct Circle, struct Rectangle >;
-
-#else
-
-void foo() {}
-void bar() { foo(); }
-
-// int var_test;
-// void fun_test() {
-//     ++var_test;
-// }
-// __attribute__((constructor(101)))
-// void func_test() {}
-
-#endif
-
-#endif
