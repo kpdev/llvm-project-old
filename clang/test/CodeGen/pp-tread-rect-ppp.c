97537beb47093e399ecc22e6073a744120cd4c83
Pavel Kosov <kpdev42@gmail.com>
2024-02-08 11:45:07 +0300

    [PP-EXT] Change pthread test to make it buildable

--- clang/test/CodeGen/pp-tread-rect-ppp.c
+++ clang/test/CodeGen/pp-tread-rect-ppp.c
@@ -10,27 +10,37 @@
 // аналогичная объектно-ориентированной обертке
 //==============================================================================
 
+// Прямоугольник
+typedef struct Rectangle{int x, y;} Rectangle;
+
+// Основа специализации. Прямоугольник, передаваемый потоку
+// и формируемое значение результата (периметр)
+typedef struct RectPreimeter {Rectangle r; double p;} RectPreimeter;
+
+
 // Обобщенная структура, используемая для обертки данных,
 // передаваемых в поточную функцию, и для возврата результата
 // Конкретные данные формируются в специализации для каждого
 // отдельного потока
-typedef struct ThreadData {pthread_t threadId;}<> ThreadData;
+typedef struct ThreadData {pthread_t threadId;}< struct RectPreimeter; > ThreadData;
 
 // Обобщенная функция, задающая формат для передачи аргументов
 // и подменяемая на соответствующий обработчик специализации.
 // Часто он может бытьтолько один
-void RunThread <ThreadData* d>() = 0;
+void RunThread <struct ThreadData* d>() {};// = 0;
 
 // POSIX функция, вызвывающая обработчик обобщения
 // подменяемый обработчиком специализации
 void* ThreadFunc(void* d) {
-  RunThread<(Thread*) d>();
+  struct Thread* pt = (struct Thread*) d;
+  // RunThread<(struct Thread*) d>();
+  RunThread<pt>();
   return NULL;
 }
 
 // Функция создания и запуска потока
 int StartThread(ThreadData* td) {
-  return pthread_create(td->threadId, NULL, ThreadFunc, td);
+  return pthread_create(&td->threadId, NULL, ThreadFunc, td);
 }
 
 // Функция ожидания завершения потока
@@ -45,12 +55,12 @@ int WaitThread(ThreadData* td) {
 // В примере только прямоугольник.
 //==============================================================================
 
-// Прямоугольник
-typedef struct Rectangle{int x, y;} Rectangle;
+// // Прямоугольник
+// typedef struct Rectangle{int x, y;} Rectangle;
 
-// Основа специализации. Прямоугольник, передаваемый потоку
-// и формируемое значение результата (периметр)
-typedef struct RectPreimeter {Rectangle r; double p;} RectPreimeter;
+// // Основа специализации. Прямоугольник, передаваемый потоку
+// // и формируемое значение результата (периметр)
+// typedef struct RectPreimeter {Rectangle r; double p;} RectPreimeter;
 
 // Вывод результата вычислений периметра конкретно прямоугольника
 void PrintRectPerimeter(RectPreimeter* rp, const char* str) {
@@ -58,12 +68,14 @@ void PrintRectPerimeter(RectPreimeter* rp, const char* str) {
 }
 
 // Специализация для потока
-ThreadData + <RectPreimeter;>;
+// ThreadData + <RectPreimeter;>;
 
 // Обработчик специализации, запускаемый в потоке
 // вычисляет периметр прямоугольника.
-void RunThread<ThreadData<RectPreimeter> *rp>() {
-  rp->p = (double)((rp->r.x+rp.r->y)*2);
+void RunThread<struct ThreadData<struct RectPreimeter> *rp>() {
+  struct RectPreimeter* tmp = &rp->@;
+  tmp->p = (double)((tmp->r.x + tmp->r.y)*2);
+  // rp->@ = (double)((rp->@r.x+rp->@r.y)*2);
 }
 
 
@@ -76,9 +88,26 @@ int main () {
     // Если инициализация напрямую еще не работает.
     // Ее можно заменить на присваивания.
     // Представлен вариант инициализации не всех полей. Может его и не будет.
-    ThreadData<RectPreimeter> thread1 = {0}<{3,5,0.0}>;
-    ThreadData<RectPreimeter> thread2 = {0}<{7,4,0.0}>;
-    ThreadData<RectPreimeter> thread3 = {0}<{6,8,0.0}>;
+    // struct ThreadData<struct RectPreimeter> thread1 = {0}<{3,5,0.0}>;
+    struct ThreadData<struct RectPreimeter> thread1;
+    thread1.threadId = 0;
+    thread1.@r.x = 3;
+    thread1.@r.x = 5;
+    thread1.@p = 0.0;
+
+    // struct ThreadData<struct RectPreimeter> thread2 = {0}<{7,4,0.0}>;
+    struct ThreadData<struct RectPreimeter> thread2;
+    thread2.threadId = 0;
+    thread2.@r.x = 7;
+    thread2.@r.x = 4;
+    thread2.@p = 0.0;
+
+    // struct ThreadData<struct RectPreimeter> thread3 = {0}<{6,8,0.0}>;
+    struct ThreadData<struct RectPreimeter> thread3;
+    thread3.threadId = 0;
+    thread3.@r.x = 6;
+    thread3.@r.x = 8;
+    thread3.@p = 0.0;
 
     // Далее запуск трех потоков
     StartThread(&thread1);
@@ -91,9 +120,9 @@ int main () {
     WaitThread(&thread3);
 
     // Печать периметров
-    PrintRectPerimeter(&(tread1.@), "Tread1");
-    PrintRectPerimeter(&(tread2.@), "Tread2");
-    PrintRectPerimeter(&(tread3.@), "Tread3");
+    PrintRectPerimeter(&(thread1.@), "Thread1");
+    PrintRectPerimeter(&(thread2.@), "Thread2");
+    PrintRectPerimeter(&(thread3.@), "Thread3");
 
     return 0;
 }
