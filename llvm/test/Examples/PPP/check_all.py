2b6bc03c430e7dbe73dca1571f4910a5d839c2ae
Pavel Kosov <kpdev42@gmail.com>
2024-03-23 18:29:41 +0300

    [PP-EXT][Test] Set proper CMAKE_C_COMPILER in check_all script

--- llvm/test/Examples/PPP/check_all.py
+++ llvm/test/Examples/PPP/check_all.py
@@ -2,8 +2,17 @@
 
 import os
 
+cur_dir = os.getcwd()
+path_to_clang = cur_dir + "/build/bin/clang-15"
+
+if not os.path.isfile(path_to_clang):
+    print("[ERROR] Clang not found: " + path_to_clang)
+    exit()
+
 cd_to_evol_str = "cd ./llvm/test/Examples/PPP/evolution/"
-run_tools_str = "/build && cmake --fresh -S ../ -B . "\
+run_tools_str = "/build && cmake "\
+"-DCMAKE_C_COMPILER=" + path_to_clang + " "\
+"--fresh -S ../ -B . "\
 "&& make && ../bin/evo-demo ../data/input.txt ../data/output.txt"
 
 command_list = [
@@ -25,6 +34,10 @@ command_list = [
     ,
     cd_to_evol_str
         + "02-new-func/ppp/02-ppp-type-c"
+        + run_tools_str
+    ,
+    cd_to_evol_str
+        + "03-new-field/ppp/03-ppp-type-dyn-decorator-c"
         + run_tools_str
 ]
 
