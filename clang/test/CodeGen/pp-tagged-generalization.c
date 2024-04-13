9ea91a87e9ccbd7460b4c6cd11adcb6f579a4517
Pavel Kosov <kpdev42@gmail.com>
2024-04-13 11:19:18 +0300

    [PP-EXT][Test] Add test for tagged monomethod

--- clang/test/CodeGen/pp-tagged-generalization.c
+++ clang/test/CodeGen/pp-tagged-generalization.c
@@ -1,9 +1,16 @@
 // RUN: %clang -S -emit-llvm %s 2>&1 -o - | FileCheck %s -check-prefix=CHECK-IR
+// RUN: %clang %s -o %S/a.out && %S/a.out | FileCheck %s -check-prefix=CHECK-RT && rm %S/a.out
 
-
+#include <stdio.h>
 
 struct Circle { int c; };
 struct Figure { int f; } < t0 : Circle; >;
+void PrintFigure<Figure* f>() {
+    printf("PrintFigure<Figure* f> [Default]\n");
+}
+void PrintFigure<Figure<t0>* f>() {
+    printf("PrintFigure<Figure<t0>* f> [Specialized]\n");
+}
 
 void foo()
 {
@@ -11,4 +18,12 @@ void foo()
 // CHECK-IR: %__pp_specialization_type = getelementptr inbounds %struct.Figure, ptr %__pp_head, i32 0, i32 1
 // CHECK-IR: store i32 %0, ptr %__pp_specialization_type, align 4
     struct Figure<t0> ttt;
+}
+
+// CHECK-RT: PrintFigure<Figure<t0>* f> [Specialized]
+
+int main() {
+    struct Figure<t0> ttt;
+    PrintFigure<&ttt>();
+    return 0;
 }
